00c8d63b6c56a3c1e68ba8a82f45b1bdfbdbe392
iulica04 <ivreme12@gmail.com>
2025-03-28 12:06:31 +0200

    add start menu

---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class StartGame : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        Debug.Log("Loading Level 1");
+        SceneManager.LoadScene("Level 1"); 
+    }
+}
