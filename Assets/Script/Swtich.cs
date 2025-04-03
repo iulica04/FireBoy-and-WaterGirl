d0cbc7b41e49a052515faac3d288df63fa8c7b4e
alexarama <alexarama@yahoo.com>
2025-04-04 01:36:56 +0300

    Levels

--- Assets/Script/Swtich.cs
+++ Assets/Script/Swtich.cs
@@ -4,44 +4,26 @@ using UnityEngine;
 
 public class Swtich : MonoBehaviour
 {
- 
-    [SerializeField] private Lift_For_Swtich_Y[] liftsY; // Lifturi care se mișcă pe Y
-    [SerializeField] private Lift_For_Swtich_X[] liftsX; // Lifturi care se mișcă pe X
-    [SerializeField] private Bridge b;
+    [SerializeField] Lift2 l;
+    [SerializeField] Lift1_Level4 l1;
+    [SerializeField] Bridge b;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-
-        // Activează toate lifturile pe Y
-        foreach (var lift in liftsY)
-        {
-            if (lift != null) lift.moveLift = true;
-        }
-
-        // Activează toate lifturile pe X
-        foreach (var lift in liftsX)
-        {
-            if (lift != null) lift.moveLift = true;
-        }
-
-        if (b != null) b.moveLift = true;
+        if(l!=null)
+            l.moveLift = true;
+        if (b != null)
+            b.moveLift = true;
+        if (l1 != null)
+            l1.moveLift = true;
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
-
-        // Dezactivează toate lifturile pe Y
-        foreach (var lift in liftsY)
-        {
-            if (lift != null) lift.moveLift = false;
-        }
-
-        // Dezactivează toate lifturile pe X
-        foreach (var lift in liftsX)
-        {
-            if (lift != null) lift.moveLift = false;
-        }
-
-        if (b != null) b.moveLift = false;
+        if (l != null)
+            l.moveLift = false;
+        if (b != null)
+            b.moveLift = false;
+        if (l1 != null)
+            l1.moveLift = false;
     }
 }
